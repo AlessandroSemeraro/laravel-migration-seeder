72d7a4cb03c3ddb9c50dad49782a9b7d7fa3f41d
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-02-04 18:39:09 +0100

    inizializzazione , created table from terminal

---
+++ database/migrations/2024_02_04_171653_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_company', 30);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->integer('code_train')->unique();
+            $table->string('carriage_number', 2);
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancellate')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
